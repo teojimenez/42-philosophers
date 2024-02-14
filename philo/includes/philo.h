98405038011896b63160afbf4437d34469fe8254
Teo Jimenez <teojimbru@gmail.com>
2024-02-14 07:36:15 +0100

    add structure

---
+++ philo/includes/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: teojimen <teojimen@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 07:18:07 by teojimene         #+#    #+#             */
+/*   Updated: 2024/02/14 07:18:07 by teojimene        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include "philo.h"
+# include <stdio.h>
+# include <stdlib.h> //free
+# include <unistd.h> //sleep
+# include <sys/time.h>
+# include <pthread.h>
+
+// FUNCTIONS
+
+
+#endif
