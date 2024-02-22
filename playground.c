43bb3ab0b51651f6b297dd635a4ed4a3ccc5a563
Teo Jimenez <teojimen@cbr8s4.42barcelona.com>
2024-02-22 13:55:00 +0100

    update 22/02

--- playground.c
+++ playground.c
@@ -17,6 +17,20 @@ void  *routine()
   return (NULL);
 }
 
+u_int64_t	init_time()
+{
+	struct timeval	tv;
+	if (gettimeofday(&tv, NULL) == -1)
+	{
+		write(2, "ERROR!", 7);
+		return (0);
+	}
+	// printf("Seconds since epoch: %li\n", tv.tv_sec);
+  //   printf("Microseconds: %u\n", tv.tv_usec);
+	// printf("%li", (tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
 int main()
 {
   pthread_t tid1, tid2;
@@ -25,8 +39,12 @@ int main()
   pthread_create(&tid1, NULL, &routine, NULL);
   pthread_create(&tid2, NULL, &routine, NULL);
 
+  // pthread_detach(tid1);
+  // pthread_detach(tid2);
+  printf("BEFORE-> %llu\n", init_time());
   pthread_join(tid1, NULL);
   pthread_join(tid2, NULL);
   pthread_mutex_destroy(&lock);
   printf("cont: %d\n", cont);
+  printf("AFTER->  %llu\n", init_time());
 }
